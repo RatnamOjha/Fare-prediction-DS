89edc1868bb3470303d0a42e7c7911a4a7702cbc
Ratnam Ojha <ratnamojha71@gmail.com>
2025-07-30 20:10:20 +0530

    changes to app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def predict_datapoint():
             return render_template('home.html', results=f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, port=8000)
